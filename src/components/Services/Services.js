a758295efe646584edf55d7378e3bf268fb80d44
Anik-the-dev <niksctg00@gmail.com>
2022-04-17 21:33:30 +0600

    service section in Home Page

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import useServices from '../hooks/useServices';
 import SingleService from './SingleService';
+import './Services.css'
 
 const Services = () => {
     const [service,] = useServices()
     return (
         <div className='container'>
-            <h2>My SEO Services</h2>
-            <div className='grid grid-three-col'>
-                {
-                    service.map(item => <SingleService
-                    key={item.id} singleItem={item}></SingleService>)
-                }
+            <div className='my-5'>
+                <h2 className='text-center my-4 lg'>My SEO Services</h2>
+                <div className='grid grid-three-col'>
+                    {
+                        service.map(item => <SingleService
+                            key={item.id} singleItem={item}></SingleService>)
+                    }
+                </div>
+
             </div>
+
 
         </div>
     );
