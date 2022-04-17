578473188a918c30bb9cd9cef4e8c8dde71a6ef8
Anik-the-dev <niksctg00@gmail.com>
2022-04-17 22:42:44 +0600

    about me page done without alignment

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AboutMe from './AboutMe';
 
 const About = () => {
     return (
         <div>
             <h1>This is About</h1>
+            <AboutMe></AboutMe>
         </div>
     );
 };
---
+++ src/components/About/AboutMe.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './About.css'
+
+const AboutMe = () => {
+    return (
+        <div className='container'>
+            <div className='my-5  grid about'>
+                <div className='aboutme-description'>
+                    <h1>I am Anik Acharjee - Web Developer & SEO Consultant.</h1>
+                    <p>My passion for technical SEO helps over 200+ clients to optimize their websites. <br/>
+                        I worked as a lead Technical SEO manager for Wedevs from 2018 to 2020.<br/>
+                        I helped a retail shop 'duds fashion' to grow their traffic by more than 550% in 2019-2020.<br/>
+                        My Strength defines my honesty at work. I'm more than a helping hand.</p>
+
+                </div>
+                <div className='aboutme-photo'>
+                    <img className='rounded' src='anik9.png' alt='anik'></img>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default AboutMe;
