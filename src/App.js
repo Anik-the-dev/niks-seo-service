26d6c377eef75b5705e10baa773c4261c2fb2964
Anik-the-dev <niksctg00@gmail.com>
2022-04-17 13:27:14 +0600

    header section done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './css/style.css';
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 
 const Header = () => {
     return (
-        <div>
+        
+        <div class="navbar">
+            <div class="container flex">
+                <h1 class="logo">nikseo.</h1>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/services'>Services</Link></li>
+                    <li><Link to='/blog'>Blog</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                    <li><Link to='/signup'>Signup</Link></li>
 
-            
+                </ul>
+            </div>
         </div>
     );
 };
