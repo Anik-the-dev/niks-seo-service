92503f2510f64d42a8303a8b5d2e07ac4e5fe0c1
Anik-the-dev <niksctg00@gmail.com>
2022-04-18 00:40:36 +0600

    sign up css done

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import '../Banner/Banner.css';
 const SignUp = () => {
     return (
-        <div>
-            
+        <div className='container'>
+        <div className='login grid'>
+            <div className='p-2'>
+                <h2>Login Here for Checkout Services.</h2>
+            </div>
+            <div>
+                <div className="showcase-form card">
+                    <h2>Sign Up Here for Checkout.</h2>
+
+                    <form>
+                        <div className="form-control">
+                            <input type="text" name="name" placeholder="Name" required></input>
+                        </div>
+                        <div className="form-control">
+                            <input type="email" name="email" placeholder="Email" required></input>
+                        </div>
+                        <div className="form-control">
+                            <input type="password" name="password" placeholder="Password" required></input>
+                        </div>
+                        <input type="submit" value="Sign Up" className="btn btn-primary w-100 p-2"></input>
+                    </form>
+                </div>
+            </div>
         </div>
+
+    </div>
     );
 };
 
