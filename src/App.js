cf68409379424e8c405112e35a890d55e4596655
Anik-the-dev <niksctg00@gmail.com>
2022-04-17 12:58:53 +0600

    components Created, Route Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import About from './components/About/About';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import Services from './components/Services/Services';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route  path='/' element={<Home/>}></Route>
+        <Route  path='/home' element={<Home/>}></Route>
+        <Route  path='/blog' element={<Blog/>}></Route>
+        <Route  path='/about' element={<About/>}></Route>
+        <Route  path='/services' element={<Services/>}></Route>
+        <Route  path='/login' element={<Login/>}></Route>
+        <Route  path='/signup' element={<SignUp/>}></Route>
+        <Route  path='*' element={<NotFound/>}></Route>
+
+      </Routes>
+
+
     </div>
   );
 }
