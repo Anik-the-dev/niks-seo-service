ffe4f1a9281522b1a1036518a0a4000992a9ea97
Anik-the-dev <niksctg00@gmail.com>
2022-04-18 00:32:55 +0600

    login form done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,28 +4,28 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <section class="showcase">
-            <div class="container grid">
-                <div class="showcase-text">
+        <section className="showcase">
+            <div className="container grid">
+                <div className="showcase-text">
                     <h1>Boost Google Ranking's with Me.</h1>
                     <p>I am Anik Acharjee a 3 years experienced SEO Consulted helped more than 75 companies to grow their search engine ranking in last 2 years.</p>
-                    <Link class="btn btn-outline" to='/about'>About Me</Link>
+                    <Link className="btn btn-outline" to='/about'>About Me</Link>
                 </div>
 
-                <div class="showcase-form card">
+                <div className="showcase-form card">
                     <h2>Request a <span className='emphasize-text'> FREE. </span> SEO Audit</h2>
 
                     <form>
-                        <div class="form-control">
+                        <div className="form-control">
                             <input type="text" name="name" placeholder="Name" required></input>
                         </div>
-                        <div class="form-control">
+                        <div className="form-control">
                             <input type="text" name="company" placeholder="Company Name" required></input>
                         </div>
-                        <div class="form-control">
+                        <div className="form-control">
                             <input type="email" name="email" placeholder="Email" required></input>
                         </div>
-                        <input type="submit" value="Send" class="btn btn-primary"></input>
+                        <input type="submit" value="Send" className="btn btn-primary"></input>
                     </form>
                 </div>
             </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,31 @@
-
 import React from 'react';
+import '../Banner/Banner.css'
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='login grid'>
+                <div className='p-2'>
+                    <h2>Login Here for Checkout Services.</h2>
+                </div>
+                <div>
+                    <div className="showcase-form card">
+                        <h2>Login Here for Checkout Services.</h2>
+
+                        <form>
+                            <div className="form-control">
+                                <input type="email" name="email" placeholder="Email" required></input>
+                            </div>
+                            <div className="form-control">
+                                <input type="password" name="password" placeholder="Password" required></input>
+                            </div>
+                            <input type="submit" value="Login" className="btn btn-primary w-100 p-2"></input>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
